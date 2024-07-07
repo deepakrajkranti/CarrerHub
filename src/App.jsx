48313c02e06bcaa01ee3ad031caee3c943e0360c
Deepak Raj <88797436+deepakrajkranti@users.noreply.github.com>
2024-07-07 12:57:29 +0530

    Repo Setup for CarrerHub (#2)

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,27 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Home from './Page/Home'
-
+import Jobs from './Page/Jobs'
+import Community from './Page/Community'
+import { 
+  BrowserRouter as Router, 
+  Routes, 
+  Route 
+} from "react-router-dom"; 
 function App() {
 
   return (
     <>
-      <div>HI </div>
-      <Home/>
-    </>
+      <Router> 
+                <Routes> 
+                    <Route path="/" element={<Home/>} /> 
+                    <Route path="jobs" element ={<Jobs/>}/>
+                    <Route path="tutorials" element ={<Jobs/>}/>
+                    <Route path="community" element ={<Community/>}/>
+                </Routes> 
+      </Router>  
+      
+  </>
   )
 }
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,24 +1,49 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
 const Container = styled.div`
 height:40px;
 width:100vw;
-background-color: red;
-
+color: black;
+font-size:1.2em;
+font-weight: bold;
 `
 const Navbardiv= styled.div`
 display:flex;
-
+height:100%;
 `
 const Left=styled.div`
 flex:4;
 display:flex;
 justify-content:space-around;
+text-align: center;
+
 
 `
 const Right=styled.div`
 flex:1;
+display:flex;
+align-items:center;
+justify-content:space-around;
+`
+const Section = styled.div`
+display:flex;
+align-items:center;
+button{
+  padding:0.7rem;
+  font-family: inherit;
+  font-weight: 400;
+  border-radius: 6px;
+  color:white;
+  border:none;
+  &:hover{
+    border:none;
+    transition: 0.3s ease-out;
+  }
+}
+
+
 `
 
 
@@ -27,12 +52,14 @@ export const Navbar = () => {
         <Container>
         <Navbardiv>
         <Left>
-            <p>Tutorials</p>
-            <p>Jobs</p>
-            <p>Education</p>
+            <Section><Link to="jobs">Tutorials</Link></Section>
+            <Section><Link to="jobs">Jobs</Link></Section>
+            <Section><Link to="community">Community</Link></Section>
+            <Section><Link to="community">Interview</Link></Section>
         </Left>
         <Right>
-            sign in
+        <Section><button><Link to="jobs">Sign-In</Link></button></Section>
+        <Section><button><Link to="jobs">Ask Question</Link></button></Section>
         </Right>
         </Navbardiv>
         </Container>
